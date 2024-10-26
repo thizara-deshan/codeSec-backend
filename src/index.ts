a30c34dd4b236da2014c0b39aa35c64faad25fe8
Thizara <thizaradeshan@gmail.com>
2024-10-26 06:04:16 +0530

    cors error2

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "https://code-sec-frontend.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 
