60d7000fc9b8e60b6363e5469f38be1aa652275c
Thizara <thizaradeshan@gmail.com>
2024-10-26 06:06:38 +0530

    cors error3

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
